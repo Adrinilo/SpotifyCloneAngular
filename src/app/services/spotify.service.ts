cb427c45c42f1288ba45e12baa5cf7aa32e8dcd8
Adrián <93315833+Adrinilo@users.noreply.github.com>
2024-10-08 14:59:25 +0200

    Expire-handle
    Token expire error handle

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SpotifyService {
   private accessToken = localStorage.getItem('spotify_access_token');
-  private userId = localStorage.getItem('user_id');
-
+  //private userId = localStorage.getItem('user_id');
   private apiUrl = 'https://api.spotify.com/v1/';
+
+  constructor(private authService: AuthService) {}
 
   public async getUserProfile() {
     const response = await fetch(this.apiUrl + 'me', {
@@ -30,12 +32,34 @@ export class SpotifyService {
 
   public async getUserPlaylists() {
     const limit = 50;
-    const response = await fetch(this.apiUrl + 'me/playlists?limit=' + limit, {
-      headers: {
-        Authorization: `Bearer ${this.accessToken}`,
-      },
-    });
-    return await response.json();
+    try {
+      const response = await fetch(
+        this.apiUrl + 'me/playlists?limit=' + limit,
+        {
+          headers: {
+            Authorization: `Bearer ${this.accessToken}`,
+          },
+        }
+      );
+
+      // Verificamos la respuesta
+      if (!response.ok) {
+        // Si el error es 401 (Unauthorized), llamamos a logout()
+        // ya que el token ha expirado
+        if (response.status === 401) {
+          this.authService.logout();
+        }
+        // Lanzamos un error para manejar otros posibles errores
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+      }
+
+      // Si la respuesta es correcta devolvemos los datos obtenidos
+      return await response.json();
+    } catch (error) {
+      // En caso de cualquier error, lo manejamos aquí
+      console.error('Error al obtener las playlists:', error);
+      throw error;
+    }
   }
 
   public async getTracksPlaylists(playlistId: string) {
