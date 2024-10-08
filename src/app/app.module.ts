563044dc393bf3abdb68e6167f40fc052e856428
Adrián <93315833+Adrinilo@users.noreply.github.com>
2024-10-08 13:37:50 +0200

    Login-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
 import { CallbackComponent } from './components/callback/callback.component';
 
 export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: 'callback', component: CallbackComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard]  },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
@@ -16,6 +18,7 @@ export const routes: Routes = [
   declarations: [
     AppComponent,
     HomeComponent,
+    LoginComponent,
     CallbackComponent,
   ],
   imports: [BrowserModule, HttpClientModule, RouterModule.forRoot(routes)],
