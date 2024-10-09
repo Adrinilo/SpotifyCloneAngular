8a78a0984a50084d7bdeb952637cdeb023258fcf
Adrián <93315833+Adrinilo@users.noreply.github.com>
2024-10-09 12:15:04 +0200

    HideNavbar
    Hide on Login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'platinas';
+  hideNavbar = false; // Controla la visibilidad del navbar
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) { // NavigationEnd indica que la navegación ha llegado a su fin
+        this.hideNavbar = event.url === '/login';
+      }
+    });
+  }
 }
