2b736ac2641d66b24e1170a1baef40a31e53f417
Adrián <93315833+Adrinilo@users.noreply.github.com>
2024-12-09 14:56:33 +0100

    Update spotify-scopes.ts
    Refactor

--- src/app/constants/spotify-scopes.ts
+++ src/app/constants/spotify-scopes.ts
@@ -1,21 +1,28 @@
 export const SPOTIFY_SCOPES = {
-  userProfile: [
-    'user-read-private', // Permite leer información privada del perfil del usuario.
-    'user-read-email', // Permite acceder a la dirección de correo electrónico del usuario.
+  connect: [
+    'user-read-playback-state', // Permite acceder al estado de reproducción.
+    'user-modify-playback-state' // Permite controlar la reproducción.
   ],
   playback: [
-    'user-read-playback-state', // Permite acceder al estado de reproducción del usuario.
-    'user-read-recently-played', // Permite ver las canciones reproducidas recientemente por el usuario.
-    'user-modify-playback-state' // Permite controlar la reproducción del usuario.
+    'streaming', // Permite la reproducción de una canción.
   ],
   playlists: [
     'playlist-read-private', // Permite acceder a listas de reproducción privadas del usuario.
+  ],
+  history: [
+    'user-read-recently-played', // Permite ver las canciones reproducidas recientemente por el usuario.
+  ],
+  users: [
+    'user-read-email', // Permite acceder a la dirección de correo electrónico del usuario.
+    'user-read-private', // Permite leer información privada del perfil del usuario.
   ],
 };
 
 // Exporta los scopes como un string separado por espacios
 export const SPOTIFY_SCOPES_STRING = [
-  ...SPOTIFY_SCOPES.userProfile,
+  ...SPOTIFY_SCOPES.connect,
   ...SPOTIFY_SCOPES.playback,
   ...SPOTIFY_SCOPES.playlists,
+  ...SPOTIFY_SCOPES.history,
+  ...SPOTIFY_SCOPES.users,
 ].join(' ');
